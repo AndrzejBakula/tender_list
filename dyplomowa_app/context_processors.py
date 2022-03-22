fa2a7d15a6546fff18cee62feac15630641cc59b
AndrzejBakula <goldenwroc@gmail.com>
2022-03-22 20:49:08 +0100

    tender date added

--- dyplomowa_app/context_processors.py
+++ dyplomowa_app/context_processors.py
@@ -1,6 +1,8 @@
 from datetime import date, timedelta, timezone
+from django.utils import timezone
 
 
 def date_processor(request):
-    global_date = date.today()
+    part_date = timezone.localtime(value=None, timezone=None).date()
+    global_date = f"{part_date.strftime('%A')} {part_date}"
     return {"global_date": global_date}
